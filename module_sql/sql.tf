e36976cc03f1d3897ef292d3b811fd54ca05aa2d
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-11 22:30:17 -0500

    Create sql.tf

---
+++ module_sql/sql.tf
@@ -0,0 +1,9 @@
+module "sql-db" {
+  source  = "GoogleCloudPlatform/sql-db/google"
+  version = "19.0.0"
+  
+  project_id = var.projectid
+  name = var.name
+  database_version = var.database_version
+  deletion_protection = var.delation_protection
+}
